abbbf7267f61a041736c073501a5bfe625e4b1f8
Ricardo Armenta <R3XED@outlook.es>
2023-11-13 10:42:34 -0700

    Initial commit

---
+++ src/main/java/com/Canitas02/FralarmSecurity/FralarmSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.Canitas02.FralarmSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FralarmSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FralarmSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Canitas02/FralarmSecurity/FralarmSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Canitas02.FralarmSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FralarmSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
