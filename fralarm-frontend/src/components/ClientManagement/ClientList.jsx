6d684c2f2eb4c851853a440977ab4f1f08b8bec4
Canitas <r3xed@outlook.es>
2023-11-13 23:16:04 -0700

    clien list updated

--- fralarm-frontend/src/components/ClientManagement/ClientList.jsx
+++ fralarm-frontend/src/components/ClientManagement/ClientList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchClients } from '../../services/clientService';
 
-
 const ClientList = () => {
     const [clients, setClients] = useState([]);
 
@@ -15,15 +14,54 @@ const ClientList = () => {
     }, []);
 
     return (
-        <div>
-            {clients.length ? (
-                <ul>
+        <div className="overflow-hidden shadow-md sm:rounded-lg">
+            {clients.length > 0 ? (
+                <table className="min-w-full leading-normal">
+                    <thead>
+                    <tr>
+                        <th className="px-5 py-3 border-b-2 border-blue-200 bg-blue-100 text-left text-xs font-semibold text-blue-600 uppercase tracking-wider">
+                            Name
+                        </th>
+                        <th className="px-5 py-3 border-b-2 border-blue-200 bg-blue-100 text-left text-xs font-semibold text-blue-600 uppercase tracking-wider">
+                            Email
+                        </th>
+                        <th className="px-5 py-3 border-b-2 border-blue-200 bg-blue-100 text-left text-xs font-semibold text-blue-600 uppercase tracking-wider">
+                            Address
+                        </th>
+                        <th className="px-5 py-3 border-b-2 border-blue-200 bg-blue-100 text-left text-xs font-semibold text-blue-600 uppercase tracking-wider">
+                            Phone Number
+                        </th>
+                        <th className="px-5 py-3 border-b-2 border-blue-200 bg-blue-100 text-left text-xs font-semibold text-blue-600 uppercase tracking-wider">
+                            Company
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
                     {clients.map(client => (
-                        <li key={client.id}>{client.name}</li> // Replace 'client.name' with actual data fields
+                        <tr key={client.id}>
+                            <td className="px-5 py-5 border-b border-blue-200 bg-white text-sm">
+                                <p className="text-gray-900 whitespace-no-wrap">{client.name}</p>
+                            </td>
+                            <td className="px-5 py-5 border-b border-blue-200 bg-white text-sm">
+                                <p className="text-gray-900 whitespace-no-wrap">{client.email}</p>
+                            </td>
+                            <td className="px-5 py-5 border-b border-blue-200 bg-white text-sm">
+                                <p className="text-gray-900 whitespace-no-wrap">{client.address}</p>
+                            </td>
+                            <td className="px-5 py-5 border-b border-blue-200 bg-white text-sm">
+                                <p className="text-gray-900 whitespace-no-wrap">{client.phoneNumber}</p>
+                            </td>
+                            <td className="px-5 py-5 border-b border-blue-200 bg-white text-sm">
+                                <p className="text-gray-900 whitespace-no-wrap">{client.company}</p>
+                            </td>
+                        </tr>
                     ))}
-                </ul>
+                    </tbody>
+                </table>
             ) : (
-                <p>No clients found.</p>
+                <div className="px-5 py-5 bg-white text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap">No clients found.</p>
+                </div>
             )}
         </div>
     );
--- fralarm-frontend/src/components/ClientManagement/ClientManagementPage.jsx
+++ fralarm-frontend/src/components/ClientManagement/ClientManagementPage.jsx
@@ -4,18 +4,25 @@ import ClientList from './ClientList';
 
 const ClientManagementPage = () => {
     return (
-        <div className="min-h-screen bg-gray-100 text-gray-900">
+        <div className="min-h-screen bg-blue-50 text-gray-900">
             <div className="container mx-auto px-4 sm:px-8 max-w-3xl">
                 <div className="py-8">
-                    <div>
-                        <h2 className="text-2xl font-semibold leading-tight">Clients</h2>
+                    <div className="mb-4">
+                        <h2 className="text-3xl font-semibold text-blue-700 leading-tight">Client Management</h2>
+                    </div>
+
+                    {/* Action buttons or additional controls could be added here */}
+                    <div className="flex justify-between mb-6">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Add New Client
+                        </button>
+                        {/* Additional buttons or controls */}
                     </div>
 
                     <div className="py-4">
-                        {/* Client List Component */}
+                        {/* Client List Component with a blue theme */}
                         <ClientList />
                     </div>
-                    {/* You can add more components here, like buttons for adding new clients or filters */}
                 </div>
             </div>
         </div>
